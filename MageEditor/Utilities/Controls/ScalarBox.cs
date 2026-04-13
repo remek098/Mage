47d2021414a00ce6835c70109eaef84afe5cdeba
Remigiusz Kuśmierczyk <rkusmierczyk355@gmail.com>
2026-04-13 19:59:40 +0200

    Editor: VectorBox and ScalarBox added.

---
+++ MageEditor/Utilities/Controls/ScalarBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MageEditor.Utilities.Controls
+{
+    class ScalarBox : NumberBox
+    {
+        static ScalarBox()
+        {
+            // https://learn.microsoft.com/en-us/dotnet/api/system.windows.frameworkelement.defaultstylekey?view=windowsdesktop-10.0
+            // custom style key, so that WPF knows what to apply.
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScalarBox), new FrameworkPropertyMetadata(typeof(ScalarBox)));
+        }
+    }
+}
